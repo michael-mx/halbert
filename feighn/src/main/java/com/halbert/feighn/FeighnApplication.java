3c5aaf3262608a9652a84eddfb07ead9bdf906b9
michael-mx <michael-mx@users.noreply.github.com>
2019-08-22 11:32:06 +0800

    a lot of changes

---
+++ feighn/src/main/java/com/halbert/feighn/FeighnApplication.java
@@ -0,0 +1,13 @@
+package com.halbert.feighn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeighnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeighnApplication.class, args);
+    }
+
+}
